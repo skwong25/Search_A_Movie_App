d59ec657c307461848584739c2d440c0549684a4
Suzanna <skwong25@hotmail.co.uk>
2020-07-20 14:08:39 +0100

    Updates

---
+++ precache-manifest.b2f3ac841ac383fba2024c35332d6c14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "928bedaa1818249a69a5c0ff633d2f51",
+    "url": "/Search_A_Movie_App/index.html"
+  },
+  {
+    "revision": "490117a7f61779c3e9c3",
+    "url": "/Search_A_Movie_App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5dd03ff203897eadd941",
+    "url": "/Search_A_Movie_App/static/js/2.f3f291e5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Search_A_Movie_App/static/js/2.f3f291e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "490117a7f61779c3e9c3",
+    "url": "/Search_A_Movie_App/static/js/main.577ce960.chunk.js"
+  },
+  {
+    "revision": "d78cf1f28c5f3844ea24",
+    "url": "/Search_A_Movie_App/static/js/runtime-main.8528c299.js"
+  }
+]);
